5fb960944f19dfb8daf72ecc131d7eeb096b66d4
Vladislav Vereschagin <slavaver@gmail.com>
2022-09-16 09:57:13 +0300

    Added tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://slavaver.github.io',
-    base: '/web-course-site'
+  site: 'https://slavaver.github.io',
+  base: '/web-course-site',
+  integrations: [tailwind()]
 });
