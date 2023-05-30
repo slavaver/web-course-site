f229ed0c348e95049c829930baba73836d0b5f9c
DimDimich-creator <stilys62@gmail.com>
2023-05-30 15:41:02 +0300

    feat: добавлено отслеживание положение заголовков

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://slavaver.github.io',
-  base: '/web-course-site',
-  integrations: [tailwind()]
+    site: 'https://slavaver.github.io',
+    base: '/web-course-site',
+    integrations: [tailwind()]
 });
