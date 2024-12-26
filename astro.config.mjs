1b80853eb9af51f86864dd46768196dd6bbd7670
qalqaa <113594321+qalqaa@users.noreply.github.com>
2024-12-26 20:26:52 +0300

    dev(styles): changed tailwind to unoCSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from "astro/config";
-
-import tailwind from "@astrojs/tailwind";
+import UnoCSS from "unocss/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://slavaver.github.io",
-  base: "/web-course-site",
-  output: "static",
-  integrations: [tailwind()],
+    site: "https://slavaver.github.io",
+    base: "/web-course-site",
+    output: "static",
+    integrations: [UnoCSS({ injectReset: true })],
 });
